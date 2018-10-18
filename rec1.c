66598b1669883ba5078c87909d7e316db041121c
rbuibas <buibas.raul@gmail.com>
2018-10-19 00:02:33 +0200

    factorial with tail recursion

--- rec1.c
+++ rec1.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int recursive_printing(int);
+int factorial_tail(int, int);
+void recursive_printing(int);
 
 int main(int argc, char ** argv) {
 	if (argc == 1) printf("Please provide something to count down from.\n");
@@ -9,13 +10,19 @@ int main(int argc, char ** argv) {
 	if (argc == 2) {
 		long conv = strtol(argv[1], NULL, 10);
 		recursive_printing((int) conv);
+		printf("Factorial of %d is %d\n", (int) conv, factorial_tail(1,(int) conv));
 	}
 	return 0;
 }
 
-int recursive_printing(int countdown) {
+void recursive_printing(int countdown) {
 	if (countdown == 0)
-		return 0;
-	printf("%d\n", countdown);
+		return;
 	recursive_printing(--countdown);
+}
+
+int factorial_tail(int sum, int n) {
+	if (n == 1) return sum;
+	int newSum = sum * n;
+	factorial_tail(newSum, --n);
 }
